7ab70d462af52d1c28241c4b1b26d47daa00bda0
Peter Cai <peter@typeblog.net>
2020-03-01 14:43:57 +0800

    releasetools: do not remove dynamic partitions in system-only builds
    * Before this commit, the generated `dynamic_partitions_op_list` in
      FullOTA packages always tries to remove all partitions and recreate
      them upon flashing. This makes it impossible to have a system-only
      "FullOTA" because vendor partition(s) are always removed.
    
    * This commit detects if a build is vendor-less and disables every
      dynamic partition operation except `resize`, in order to keep the
      original content around after the flash. The change should not affect
      non-dynamic-partition or builds with vendor image included.
    
    Change-Id: I0cded7f3b2958f35103d73d19b7fb5f292f6c17f
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -824,8 +824,9 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     # be re-added.
     dynamic_partitions_diff = common.DynamicPartitionsDifference(
         info_dict=OPTIONS.info_dict,
-        block_diffs=block_diff_dict.values(),
-        progress_dict=progress_dict)
+        block_diffs=block_diffs,
+        progress_dict=progress_dict,
+        build_without_vendor=(not HasVendorPartition(input_zip)))
     dynamic_partitions_diff.WriteScript(script, output_zip,
                                         write_verify_script=OPTIONS.verify)
   else:
